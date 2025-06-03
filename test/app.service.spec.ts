337d925a6cff62a88ed48c3047097b2ed7ff7f97
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2025-06-03 14:27:33 +0200

    config tests failing on purpose + docker repo

---
+++ test/app.service.spec.ts
@@ -0,0 +1,17 @@
+import { AppService } from '../src/app.service';
+
+describe('AppService', () => {
+  let appService: AppService;
+
+  beforeEach(() => {
+    appService = new AppService();
+  });
+
+  describe('getHello', () => {
+    it('should return "Hello World!"', () => {
+      const result = 'Hello Wo!';
+
+      expect(appService.getHello()).toBe(result);
+    });
+  });
+});
