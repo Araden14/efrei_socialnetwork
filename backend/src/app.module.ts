c08328b57567b5b0ecdcc504eb387c798955d209
GuilhermeDias02 <guilherme.alex.dias@gmail.com>
2025-06-18 09:37:20 +0200

    separated redis env vars

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,10 +12,10 @@ import { RedisModule } from './redis/redis.module';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { AuthModule } from './auth/auth.module';
 
-const redisUrl = new URL(
-  `${process.env.REDIS_HOST_EXTERNAL}:${process.env.REDIS_PORT ?? 6379}`,
-);
-console.log(process.env.REDIS_HOST_EXTERNAL);
+// const redisUrl = new URL(
+//   `${process.env.REDIS_HOST_EXTERNAL}:${process.env.REDIS_PORT ?? 6379}`,
+// );
+// console.log(process.env.REDIS_HOST_EXTERNAL);
 
 @Module({
   imports: [
@@ -34,10 +34,10 @@ console.log(process.env.REDIS_HOST_EXTERNAL);
     PrismaModule,
     BullModule.forRoot({
       redis: {
-        host: redisUrl.hostname,
+        host: process.env.REDIS_HOSTNAME,
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-        port: parseInt(redisUrl.port ?? 6379, 10),
-        password: redisUrl.password,
+        port: parseInt(process.env.REDIS_PORT ?? '6379', 10),
+        password: process.env.REDIS_PASSWORD,
       },
     }),
     UsersModule,
